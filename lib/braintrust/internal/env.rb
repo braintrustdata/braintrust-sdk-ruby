5421567f6a2cee9b3d639c8123695af1c9cc082f
David Elner <david.elner@braintrustdata.com>
2025-12-19 14:50:26 -0500

    Added: Contrib#auto_instrument!

--- lib/braintrust/internal/env.rb
+++ lib/braintrust/internal/env.rb
@@ -4,6 +4,22 @@ module Braintrust
   module Internal
     # Environment variable utilities.
     module Env
+      ENV_AUTO_INSTRUMENT = "BRAINTRUST_AUTO_INSTRUMENT"
+      ENV_INSTRUMENT_EXCEPT = "BRAINTRUST_INSTRUMENT_EXCEPT"
+      ENV_INSTRUMENT_ONLY = "BRAINTRUST_INSTRUMENT_ONLY"
+
+      def self.auto_instrument
+        ENV[ENV_AUTO_INSTRUMENT] != "false"
+      end
+
+      def self.instrument_except
+        parse_list(ENV_INSTRUMENT_EXCEPT)
+      end
+
+      def self.instrument_only
+        parse_list(ENV_INSTRUMENT_ONLY)
+      end
+
       # Parse a comma-separated environment variable into an array of symbols.
       # @param key [String] The environment variable name
       # @return [Array<Symbol>, nil] Array of symbols, or nil if not set
